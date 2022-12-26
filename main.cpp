768d107faeec79bacff740dbc64f6a3b7427c7b7
Amir Asdaghpour <57164807+amirmasp@users.noreply.github.com>
2022-12-26 13:47:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@ void countFunc(int* ptrToInt, size_t times) {
    /*
     The thread t1 increments an integer n times
    */ 
- 	while(times > 0) {
-		*ptrToInt += 1;
-    times-- ;
-	} 
+   while(times > 0) {
+   	*ptrToInt += 1;
+    	times-- ;
+   } 
 }
 
 int main() { 
